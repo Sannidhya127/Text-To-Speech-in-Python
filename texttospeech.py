81159c380ba963394f493f08db5c29367a54df55
Sannidhya Dasgupta <73464131+Sannidhya127@users.noreply.github.com>
2020-10-26 13:57:38 +0530

    Add files via upload

---
+++ texttospeech.py
@@ -0,0 +1,21 @@
+from gtts import gTTS
+
+# If you don't have gtts go to the terminal and write pip install gtts
+import os
+
+# Reading the files
+f = open("3.txt", "r")
+x = f.read()
+
+# Specifying language:
+language = 'en'  # You can specify any language. Reffer to the gtts documentation
+# Working!!!!
+# simply write this down for better sound
+audio = gTTS(text=x, lang=language, slow=False)
+
+# Now save the audio file
+# You can save by any name you wish
+# When I play the file you won't be able to hear it as my recorder is not working but I am sure this code works
+# Source Code in description
+audio.save("3.wav")
+os.system("3.wav")
